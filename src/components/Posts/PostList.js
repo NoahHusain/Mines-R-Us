5ecda440d67bd084d393c9c2caece3b40921b7c7
Noah Husain <noahhusain@gmail.com>
2021-06-16 13:48:46 -0500

    Added the ability for users to delete posts. Note: currently any user can delete any post regardless of who authored it. Will add logic to prevent that once MVP has been met.

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -1,60 +1,69 @@
 import { PostContext } from "./PostProvider";
 import React, { useContext, useEffect } from "react";
-import {useHistory, Link} from 'react-router-dom'
-import "./Post.css"
+import { useHistory, Link } from "react-router-dom";
+import "./Post.css";
 
 export const PostList = () => {
+  const { getPosts, posts, deletePost } = useContext(PostContext);
+  const history = useHistory();
+  const { addPost } = useContext(PostContext);
 
-    const { getPosts, posts } = useContext(PostContext)
-    const history = useHistory()
-    const { addPost } = useContext(PostContext)
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-    useEffect(() => {
-        getPosts()
-    }, [])
- 
-    return (
-        <>
-            <h1 className="goldenRodText center">Posts</h1>
+  const handleDeletePost = (postId) => {
+    deletePost(postId)
+}
 
-            <button className='create__button' onClick={() => history.push("/Posts/create")}>
-               Found an Ore Patch?
+  return (
+    <>
+      <h1 className="goldenRodText center">Posts</h1>
+
+      <button
+        className="create__button"
+        onClick={() => history.push("/Posts/create")}
+      >
+        Found an Ore Patch?
+      </button>
+
+      {posts.map((post) => (
+        <section className="posts">
+          <article className="post" id={`post--${post.id}`}>
+            <div className="post__oreId blueText opaque">
+              Ore: {post.ore.name}
+            </div>
+            <div className="post__planetId blueText opaque">
+              Planet: {post.planet.name}
+            </div>
+            <div className="post__moonId blueText opaque">
+              {post.moon ? `Moon: ${post.moon.name}` : null}
+              {/* If moon has an ID, render the div. If not, do not render the div */}
+            </div>
+            <div className="post__landingPoint blueText opaque">
+              Landing Point: {post.landingPoint}
+            </div>
+            <div className="post__description blueText opaque">
+              Description: {post.description}
+            </div>
+            <div className="post_rockData blueText opaque">
+              Largest Rock Found: {post.rockData}
+            </div>
+            <div className="post__userId blueText opaque">
+              Posted By: {post.user.name}
+            </div>
+            <button
+              className="post" id={`post--${post.id}`}
+              onClick={(event) => {
+                event.preventDefault();
+                handleDeletePost(post.id);
+              }}
+            >
+              Delete Post
             </button>
-        
-                {
-                    posts.map(post =>
-                        <section className="posts">
-                        <article className="post" id={`post--${post.id}`}>
-
-
-                        <div className="post__oreId blueText opaque">
-                        Ore: { post.ore.name }
-                        </div>
-                        <div className="post__planetId blueText opaque">
-                        Planet: { post.planet.name}
-                        </div>
-                        <div className="post__moonId blueText opaque">
-                        { post.moon 
-                            ? `Moon: ${post.moon.name}` 
-                                : null} 
-                        {/* If moon has an ID, render the div. If not, do not render the div */}
-                        </div>
-                        <div className="post__landingPoint blueText opaque">
-                        Landing Point: { post.landingPoint }
-                        </div>
-                        <div className="post__description blueText opaque">
-                        Description: { post.description }
-                        </div>
-                        <div className="post_rockData blueText opaque">
-                        Largest Rock Found: { post.rockData }
-                        </div>
-                        <div className="post__userId blueText opaque">
-                        Posted By: { post.user.name }
-                        </div>
-                        </article> 
-                        </section>
-                    )
-                }
-        </>
-    )
+          </article>
+        </section>
+      ))}
+    </>
+  );
 }
